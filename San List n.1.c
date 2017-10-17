769f61089dd2c1450fbdfd522ef70118c0045d57
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2017-10-17 16:28:49 -0200

    Add files via upload

---
+++ San List n.1.c
@@ -0,0 +1,25 @@
+//by tonyKingNZ©
+//list of San n. 
+//Oct. 17, 2017 16:24(BRST)
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num = 0;
+    printf("Write a number(Only int numbers):\n");
+	scanf ("%d",&num);
+    if (num % 2 == 0) {
+                     
+    	printf("This number is even!\n");
+
+}
+
+    else {
+		printf("This number is odd!\n");
+		
+  }
+	  return 0;
+}
+
